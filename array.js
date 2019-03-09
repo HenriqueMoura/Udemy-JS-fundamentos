bb6eb36d7186be5f341e299c8e51933563866cc6
Henrique Almeida de Moura <henrique.almeida.moura@gmail.com>
2019-03-09 20:47:10 -0300

    Criar um Array

--- array.js
+++ array.js
@@ -4,6 +4,12 @@ console.log(valores[5])
 
 valores[4]= 10
 console.log(valores)
+console.log(valores.lemght)
 
 valores.push({id: 3}, false,null,'teste')
 console.log(valores)
+
+console.log(valores.pop())
+delete valores[0]
+console.log (valores)
+console.log(typeof valores)
