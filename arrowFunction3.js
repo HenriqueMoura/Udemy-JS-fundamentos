ee2d67ec3486110943f6e67dad079184d3250e45
Henrique Moura <henrique.almeida.moura@gmail.com>
2019-05-29 19:29:12 -0300

    Arrow com this comparação

---
+++ arrowFunction3.js
@@ -0,0 +1,13 @@
+let compararComThis =  function(param){
+    console.log(this === param)
+}
+compararComThis(global)
+
+const obj = {}
+compararComThis = compararComThis.bind(obj)
+compararComThis(global)
+compararComThis(obj)
+
+let compararComThisArrow  = param => console.log (this === param)
+compararComThis(global)
+compararComThisArrow(this)
