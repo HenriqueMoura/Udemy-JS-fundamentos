243cbe3eaeaba1050692e32b5500b6e0bf3137e6
Henrique Moura <henrique.almeida.moura@gmail.com>
2019-05-29 19:16:30 -0300

    Diminuindo codigo com

---
+++ arrowFunction1.js
@@ -0,0 +1,16 @@
+let dobro = function (a){
+    return 2 * a
+}
+dobro = (a) => {
+    return 2*a
+} 
+dobro =  a => 2*a
+console.log(dobro(Math.PI))
+
+let ola = function(){
+    return 'Olá'
+}
+ola = () => 'Olá'
+teste = () => 'teste'
+console.log(ola())
+console.log(teste())
