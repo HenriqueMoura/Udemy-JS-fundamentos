64c4d589527e53057a11e5a45669f093799b217f
Henrique Moura <henrique.almeida.moura@gmail.com>
2019-05-29 21:21:31 -0300

    Entendendo diferença entre callback e for

---
+++ callback2.js
@@ -0,0 +1,18 @@
+const notas = [ 7.7 , 5.6 ,5.2,8.9 , 3.6 , 7.1]
+//sem callback
+let notasBaixas1 = []
+for (let i in notas){
+    if (notas[i]<7){
+        notasBaixas1.push(notas[i])
+    }
+}
+console.log (notasBaixas1)
+
+// com callback
+const notasBaixas2 = notas.filter(function(nota){
+    return nota <7
+})
+    console.log(notasBaixas2)
+
+    const notasbaixas3 = notas.filter(nota => nota <7)
+    console.log (notasbaixas3)
