40dc0b2495ec3efc9906e63ff412871248338458
Henrique Moura <henrique.almeida.moura@gmail.com>
2019-06-17 14:25:50 -0300

    Ativade criando a function

---
+++ Array/forEach2.js
@@ -0,0 +1,11 @@
+Array.prototype.forEach2 = function(callback){
+        for (let i = 0; i < this.length; i++){
+            callback(this[i],i ,this)
+        }
+}
+const aprovados = ['Ana','Marcos', 'Daniel']
+
+aprovados.forEach2(function(nome,indice){
+        console.log(`${indice+1}) ${nome}`)
+        
+})
