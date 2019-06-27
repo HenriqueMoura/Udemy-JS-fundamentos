c0f9e0f17ffd748c4d122832e89536d64233432d
Henrique Moura <henrique.almeida.moura@gmail.com>
2019-06-27 18:14:41 -0300

    concat

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Ualeskan', 'Cibalena']
+const filhos = ['Uoxinton','Uesclei']
+
+const todos = filhas.concat(filhos)
+console.log(todos)
+
+console.log(['a','b'].concat([1,2],[3,4],[[6,7]]))
