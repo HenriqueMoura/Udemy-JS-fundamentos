b927a84cefd4f32ec9fb23cd314f13e718d0b243
Henrique Moura <henrique.almeida.moura@gmail.com>
2019-07-02 18:47:33 -0300

    metodo require

--- node/pastaA/pastaB/acessarModulo.js
+++ node/pastaA/pastaB/acessarModulo.js
@@ -1,2 +1,15 @@
 const moduleA = require('../../moduloA')
 console.log(moduleA.ola)
+
+
+const saudacao = require('saudacao')
+console.log(saudacao.ola)
+
+const  c =  require('./pastaC/index')
+console.log(c.ola2)
+
+// const http  = require('http')
+// http.createServer((req ,res)=>{
+//     res.write('Bom dia!')
+//     res.end()
+// }).listen()
