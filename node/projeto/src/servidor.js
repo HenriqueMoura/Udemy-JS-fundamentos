01b48856747f040a38ca24dea400c094a68f2295
Henrique Moura <henrique.almeida.moura@gmail.com>
2019-07-04 19:30:25 -0300

    leitura e escrita

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,15 @@
+const porta = 3003
+
+const express = require ('express')
+const app = express()
+
+app.get('/produtos',(req,res,next)=>{
+    res.send ({
+        nome:'Notebook',
+        preco: 123.45, //Converter para JSON
+    })
+})
+
+app.listen(porta, ()=>{
+    console.log(`Servidor executando na porta ${porta}`)
+})
